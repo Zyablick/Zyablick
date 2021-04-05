1490d86c280e207ff56cb3e595bba5efedaf4b03
Kseniia Balaenkova <kb3668@nyu.edu>
2021-04-04 22:53:35 -0400

    Try1

--- generatorsite/generator.js
+++ generatorsite/generator.js
@@ -42,3 +42,14 @@ function refresh() {
   document.getElementById('Display').innerHTML = data[RandomNumber]
 
 }
+
+var randomlinks=new Array()
+
+randomlinks[0]="http://freewarejava.com"
+randomlinks[1]="http://javascriptkit.com"
+randomlinks[2]="http://dynamicdrive.com"
+randomlinks[3]="http://cnn.com"
+randomlinks[4]="http://www.geocities.com"
+
+function randomlink(){
+window.location=randomlinks[Math.floor(Math.random()*randomlinks.length)]
