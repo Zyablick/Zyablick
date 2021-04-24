4061f156ac2020e8e624a2361e270dbe20d44586
Zyablick <kb3668@nyu.edu>
2021-04-24 14:27:59 -0400

    current updates
    -fixed links
    - added svgs
    - changed font

---
+++ generatorsitefinal/generator.js
@@ -0,0 +1,87 @@
+// randomlink
+var randomlinks=new Array()
+
+randomlinks[0]="http://freewarejava.com"
+randomlinks[1]="http://javascriptkit.com"
+randomlinks[2]="http://dynamicdrive.com"
+randomlinks[3]="http://cnn.com"
+randomlinks[4]="http://www.geocities.com"
+
+// "https://www.youtube.com/watch?v=6LIkX0U6f5k"
+// "https://www.youtube.com/watch?v=I1gMUbEAUFw"
+// "https://www.youtube.com/watch?v=U9Zj1BaH01c&list=RDI1gMUbEAUFw&index=2"
+// "https://www.youtube.com/watch?v=xU5ikYwR3QQ&list=RDI1gMUbEAUFw&index=7"
+// "https://www.youtube.com/watch?v=rpOMTJO0pQs&list=RDI1gMUbEAUFw&index=8"
+// "https://www.youtube.com/watch?v=ezTWZxnpBP4&list=PLF99-lxSbrkY3PuqaXaS-jAVP1qYfYEEy&index=2"
+
+// "https://www.youtube.com/watch?v=z4vf0wf6oBM"
+// "https://www.youtube.com/watch?v=nc88b7ogZQM"
+// "https://www.youtube.com/watch?v=9kaIT6Lu1Hk"
+// "https://www.youtube.com/watch?v=ujhOYGIvMhw"
+// "https://www.youtube.com/watch?v=SSbnDxBBvEM&list=PLKZPhIn8-5PM6v2TnKcdfd7NLqSSHGneR&index=1"
+
+// "https://www.youtube.com/watch?v=SRq7XtDW0wg"
+// "https://www.youtube.com/watch?v=AHVLT18Z1XI"
+// "https://routeshuffle.com/"
+// "https://www.youtube.com/watch?v=2L2lnxIcNmo"
+// "https://www.youtube.com/watch?v=yKItGCpL73M"
+// "https://www.youtube.com/watch?v=aPN89GOsDaI"
+// "https://www.youtube.com/watch?v=2WE-L8iyu0U"
+// "https://www.youtube.com/watch?v=EYDDtTJ6TtQ"
+
+function linkOnClick(){
+window.location=randomlinks[Math.floor(Math.random()*randomlinks.length)]
+}
+
+// dance
+var randomdance=new Array();
+
+randomdance[0]="https://www.youtube.com/watch?v=6LIkX0U6f5k"
+randomdance[1]="https://www.youtube.com/watch?v=I1gMUbEAUFw"
+randomdance[2]="https://www.youtube.com/watch?v=U9Zj1BaH01c&list=RDI1gMUbEAUFw&index=2"
+randomdance[3]="https://www.youtube.com/watch?v=xU5ikYwR3QQ&list=RDI1gMUbEAUFw&index=7"
+randomdance[4]="https://www.youtube.com/watch?v=rpOMTJO0pQs&list=RDI1gMUbEAUFw&index=8"
+randomdance[5]="https://www.youtube.com/watch?v=ezTWZxnpBP4&list=PLF99-lxSbrkY3PuqaXaS-jAVP1qYfYEEy&index=2"
+
+function danceOnClick(){
+window.location=randomdance[Math.floor(Math.random()*randomdance.length)]
+}
+
+var randomsing=new Array();
+
+randomsing[0]="https://www.youtube.com/watch?v=z4vf0wf6oBM";
+randomsing[1]="https://www.youtube.com/watch?v=nc88b7ogZQM";
+randomsing[2]="https://www.youtube.com/watch?v=9kaIT6Lu1Hk";
+randomsing[3]="https://www.youtube.com/watch?v=ujhOYGIvMhw";
+randomsing[4]="https://www.youtube.com/watch?v=SSbnDxBBvEM&list=PLKZPhIn8-5PM6v2TnKcdfd7NLqSSHGneR&index=1";
+
+
+function singOnClick(){
+window.location=randomsing[Math.floor(Math.random()*randomsing.length)]
+}
+
+var randomexercise=new Array();
+
+randomexercise[0]="https://www.youtube.com/watch?v=SRq7XtDW0wg"
+randomexercise[1]="https://www.youtube.com/watch?v=AHVLT18Z1XI"
+randomexercise[2]="https://routeshuffle.com/"
+randomexercise[3]="https://www.youtube.com/watch?v=2L2lnxIcNmo"
+randomexercise[4]="https://www.youtube.com/watch?v=yKItGCpL73M"
+randomexercise[5]="https://www.youtube.com/watch?v=aPN89GOsDaI"
+randomexercise[6]="https://www.youtube.com/watch?v=2WE-L8iyu0U"
+randomexercise[7]="https://www.youtube.com/watch?v=EYDDtTJ6TtQ"
+
+function exerciseOnClick(){
+window.location=randomexercise[Math.floor(Math.random()*randomexercise.length)]
+}
+
+var randomgame=new Array();
+
+randomgame[0]="https://zyablick.github.io/sixthsite/sixthsite.html"
+// randomgame[1]="https://www.youtube.com/watch?v=AHVLT18Z1XI"
+// randomgame[2]="https://routeshuffle.com/"
+// randomgame[3]="https://www.youtube.com/watch?v=2L2lnxIcNmo"
+
+function gameOnClick(){
+window.location=randomgame[Math.floor(Math.random()*randomgame.length)]
+}
