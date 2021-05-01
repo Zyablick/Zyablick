8958658c02ac75086f73bb03af0302fd8d689084
Kseniia Balaenkova <you@example.com>
2021-05-01 15:54:15 -0400

    final is ready

---
+++ generatorsite copy/generator.js
@@ -0,0 +1,80 @@
+// randomlink
+var randomlinks=new Array()
+
+randomlinks[0]="https://www.youtube.com/watch?v=6LIkX0U6f5k"
+randomlinks[1]="https://www.youtube.com/watch?v=I1gMUbEAUFw"
+randomlinks[2]="https://www.youtube.com/watch?v=U9Zj1BaH01c&list=RDI1gMUbEAUFw&index=2"
+randomlinks[3]="https://www.youtube.com/watch?v=xU5ikYwR3QQ&list=RDI1gMUbEAUFw&index=7"
+randomlinks[4]="https://www.youtube.com/watch?v=rpOMTJO0pQs&list=RDI1gMUbEAUFw&index=8"
+randomlinks[5]="https://www.youtube.com/watch?v=ezTWZxnpBP4&list=PLF99-lxSbrkY3PuqaXaS-jAVP1qYfYEEy&index=2"
+randomlinks[6]="https://www.youtube.com/watch?v=z4vf0wf6oBM";
+randomlinks[7]="https://www.youtube.com/watch?v=nc88b7ogZQM";
+randomlinks[8]="https://www.youtube.com/watch?v=9kaIT6Lu1Hk";
+randomlinks[9]="https://www.youtube.com/watch?v=ujhOYGIvMhw";
+randomlinks[10]="https://www.youtube.com/watch?v=SSbnDxBBvEM&list=PLKZPhIn8-5PM6v2TnKcdfd7NLqSSHGneR&index=1";
+randomlinks[11]="https://www.youtube.com/watch?v=SRq7XtDW0wg"
+randomlinks[12]="https://www.youtube.com/watch?v=AHVLT18Z1XI"
+randomlinks[13]="https://routeshuffle.com/"
+randomlinks[14]="https://www.youtube.com/watch?v=2L2lnxIcNmo"
+randomlinks[15]="https://www.youtube.com/watch?v=yKItGCpL73M"
+randomlinks[16]="https://www.youtube.com/watch?v=aPN89GOsDaI"
+randomlinks[17]="https://www.youtube.com/watch?v=2WE-L8iyu0U"
+randomlinks[18]="https://www.youtube.com/watch?v=EYDDtTJ6TtQ"
+
+
+function linkOnClick(){
+window.location=randomlinks[Math.floor(Math.random()*randomlinks.length)]
+}
+
+// dance
+var randomdance=new Array();
+
+randomdance[0]="https://www.youtube.com/watch?v=6LIkX0U6f5k"
+randomdance[1]="https://www.youtube.com/watch?v=I1gMUbEAUFw"
+randomdance[2]="https://www.youtube.com/watch?v=U9Zj1BaH01c&list=RDI1gMUbEAUFw&index=2"
+randomdance[3]="https://www.youtube.com/watch?v=xU5ikYwR3QQ&list=RDI1gMUbEAUFw&index=7"
+randomdance[4]="https://www.youtube.com/watch?v=rpOMTJO0pQs&list=RDI1gMUbEAUFw&index=8"
+randomdance[5]="https://www.youtube.com/watch?v=ezTWZxnpBP4&list=PLF99-lxSbrkY3PuqaXaS-jAVP1qYfYEEy&index=2"
+
+function danceOnClick(){
+window.location=randomdance[Math.floor(Math.random()*randomdance.length)]
+}
+
+var randomsing=new Array();
+
+randomsing[0]="https://www.youtube.com/watch?v=z4vf0wf6oBM";
+randomsing[1]="https://www.youtube.com/watch?v=nc88b7ogZQM";
+randomsing[2]="https://www.youtube.com/watch?v=9kaIT6Lu1Hk";
+randomsing[3]="https://www.youtube.com/watch?v=ujhOYGIvMhw";
+randomsing[4]="https://www.youtube.com/watch?v=SSbnDxBBvEM&list=PLKZPhIn8-5PM6v2TnKcdfd7NLqSSHGneR&index=1";
+
+
+function singOnClick(){
+window.location=randomsing[Math.floor(Math.random()*randomsing.length)]
+}
+
+var randomexercise=new Array();
+
+randomexercise[0]="https://www.youtube.com/watch?v=SRq7XtDW0wg"
+randomexercise[1]="https://www.youtube.com/watch?v=AHVLT18Z1XI"
+randomexercise[2]="https://routeshuffle.com/"
+randomexercise[3]="https://www.youtube.com/watch?v=2L2lnxIcNmo"
+randomexercise[4]="https://www.youtube.com/watch?v=yKItGCpL73M"
+randomexercise[5]="https://www.youtube.com/watch?v=aPN89GOsDaI"
+randomexercise[6]="https://www.youtube.com/watch?v=2WE-L8iyu0U"
+randomexercise[7]="https://www.youtube.com/watch?v=EYDDtTJ6TtQ"
+
+function exerciseOnClick(){
+window.location=randomexercise[Math.floor(Math.random()*randomexercise.length)]
+}
+
+var randomgame=new Array();
+
+randomgame[0]="https://zyablick.github.io/sixthsite/sixthsite.html"
+// randomgame[1]="https://www.youtube.com/watch?v=AHVLT18Z1XI"
+// randomgame[2]="https://routeshuffle.com/"
+// randomgame[3]="https://www.youtube.com/watch?v=2L2lnxIcNmo"
+
+function gameOnClick(){
+window.location=randomgame[Math.floor(Math.random()*randomgame.length)]
+}
--- generatorsitefinal/generator.js
+++ generatorsitefinal/generator.js
@@ -1,33 +1,50 @@
+
+
+
 // randomlink
 var randomlinks=new Array()
 
-randomlinks[0]="http://freewarejava.com"
-randomlinks[1]="http://javascriptkit.com"
-randomlinks[2]="http://dynamicdrive.com"
-randomlinks[3]="http://cnn.com"
-randomlinks[4]="http://www.geocities.com"
+randomlinks[0]="https://www.youtube.com/watch?v=6LIkX0U6f5k"
+randomlinks[1]="https://www.youtube.com/watch?v=I1gMUbEAUFw"
+randomlinks[2]="https://www.youtube.com/watch?v=U9Zj1BaH01c&list=RDI1gMUbEAUFw&index=2"
+randomlinks[3]="https://www.youtube.com/watch?v=xU5ikYwR3QQ&list=RDI1gMUbEAUFw&index=7"
+randomlinks[4]="https://www.youtube.com/watch?v=rpOMTJO0pQs&list=RDI1gMUbEAUFw&index=8"
+randomlinks[5]="https://www.youtube.com/watch?v=ezTWZxnpBP4&list=PLF99-lxSbrkY3PuqaXaS-jAVP1qYfYEEy&index=2"
+randomlinks[6]="https://www.youtube.com/watch?v=z4vf0wf6oBM";
+randomlinks[7]="https://www.youtube.com/watch?v=nc88b7ogZQM";
+randomlinks[8]="https://www.youtube.com/watch?v=9kaIT6Lu1Hk";
+randomlinks[9]="https://www.youtube.com/watch?v=ujhOYGIvMhw";
+randomlinks[10]="https://www.youtube.com/watch?v=SSbnDxBBvEM&list=PLKZPhIn8-5PM6v2TnKcdfd7NLqSSHGneR&index=1";
+randomlinks[11]="https://www.youtube.com/watch?v=SRq7XtDW0wg"
+randomlinks[12]="https://www.youtube.com/watch?v=AHVLT18Z1XI"
+randomlinks[13]="https://routeshuffle.com/"
+randomlinks[14]="https://www.youtube.com/watch?v=2L2lnxIcNmo"
+randomlinks[15]="https://www.youtube.com/watch?v=yKItGCpL73M"
+randomlinks[16]="https://www.youtube.com/watch?v=aPN89GOsDaI"
+randomlinks[17]="https://www.youtube.com/watch?v=2WE-L8iyu0U"
+randomlinks[18]="https://www.youtube.com/watch?v=EYDDtTJ6TtQ"
+randomlinks[19]="http://adarkroom.doublespeakgames.com/"
+randomlinks[20]="https://orteil.dashnet.org/cookieclicker/"
+randomlinks[21]="http://www.deconstructeam.com/games/gods-will-be-watching/"
+randomlinks[22]="https://quickdraw.withgoogle.com/#"
+randomlinks[23]="http://powerline.io/"
+randomlinks[24]="https://www.youtube.com/watch?v=PGSJM-ZrZwg&list=PLF99-lxSbrkY3PuqaXaS-jAVP1qYfYEEy&index=3"
+randomlinks[25]="https://www.youtube.com/watch?v=yXO6UPOhwC0&list=PLF99-lxSbrkY3PuqaXaS-jAVP1qYfYEEy&index=4"
+randomlinks[26]="https://www.youtube.com/watch?v=GlJYTWxSVM4&list=PLF99-lxSbrkY3PuqaXaS-jAVP1qYfYEEy&index=5"
+randomlinks[27]="https://www.youtube.com/watch?v=9633daDM-UY&list=PLF99-lxSbrkY3PuqaXaS-jAVP1qYfYEEy&index=6"
+randomlinks[28]="https://www.youtube.com/watch?v=p__V39S9t_o&list=PLKZPhIn8-5PM6v2TnKcdfd7NLqSSHGneR&index=6";
+randomlinks[29]="https://www.youtube.com/watch?v=_bgCMyc5LgQ&list=PLKZPhIn8-5PM6v2TnKcdfd7NLqSSHGneR&index=3";
+randomlinks[30]="https://www.youtube.com/watch?v=eG7F0D5cbbs";
+randomlinks[31]="https://www.youtube.com/watch?v=-EUKBafuMTg";
+randomlinks[32]="https://www.youtube.com/watch?v=hav7nt_jlm0";
+randomlinks[33]="https://www.youtube.com/watch?v=Tz9d7By2ytQ"
+randomlinks[34]="https://www.youtube.com/watch?v=6fnLKyRJsrs"
+randomlinks[35]="https://www.youtube.com/watch?v=NMBuSMxrSaY"
+randomlinks[36]="https://www.thewikigame.com/group"
+randomlinks[37]="https://www.mrtroove.com/"
+randomlinks[38]="https://dinoswords.gg/"
+randomlinks[39]="https://www.linerider.com/"
 
-// "https://www.youtube.com/watch?v=6LIkX0U6f5k"
-// "https://www.youtube.com/watch?v=I1gMUbEAUFw"
-// "https://www.youtube.com/watch?v=U9Zj1BaH01c&list=RDI1gMUbEAUFw&index=2"
-// "https://www.youtube.com/watch?v=xU5ikYwR3QQ&list=RDI1gMUbEAUFw&index=7"
-// "https://www.youtube.com/watch?v=rpOMTJO0pQs&list=RDI1gMUbEAUFw&index=8"
-// "https://www.youtube.com/watch?v=ezTWZxnpBP4&list=PLF99-lxSbrkY3PuqaXaS-jAVP1qYfYEEy&index=2"
-
-// "https://www.youtube.com/watch?v=z4vf0wf6oBM"
-// "https://www.youtube.com/watch?v=nc88b7ogZQM"
-// "https://www.youtube.com/watch?v=9kaIT6Lu1Hk"
-// "https://www.youtube.com/watch?v=ujhOYGIvMhw"
-// "https://www.youtube.com/watch?v=SSbnDxBBvEM&list=PLKZPhIn8-5PM6v2TnKcdfd7NLqSSHGneR&index=1"
-
-// "https://www.youtube.com/watch?v=SRq7XtDW0wg"
-// "https://www.youtube.com/watch?v=AHVLT18Z1XI"
-// "https://routeshuffle.com/"
-// "https://www.youtube.com/watch?v=2L2lnxIcNmo"
-// "https://www.youtube.com/watch?v=yKItGCpL73M"
-// "https://www.youtube.com/watch?v=aPN89GOsDaI"
-// "https://www.youtube.com/watch?v=2WE-L8iyu0U"
-// "https://www.youtube.com/watch?v=EYDDtTJ6TtQ"
 
 function linkOnClick(){
 window.location=randomlinks[Math.floor(Math.random()*randomlinks.length)]
@@ -36,12 +53,20 @@ window.location=randomlinks[Math.floor(Math.random()*randomlinks.length)]
 // dance
 var randomdance=new Array();
 
+// randomdance[0]="file:///Users/kseniiabalaenkova/Desktop/Web%20art%20as%20site/Zyablick.github.io/generatorsitefinal/dancepage.html"
 randomdance[0]="https://www.youtube.com/watch?v=6LIkX0U6f5k"
 randomdance[1]="https://www.youtube.com/watch?v=I1gMUbEAUFw"
 randomdance[2]="https://www.youtube.com/watch?v=U9Zj1BaH01c&list=RDI1gMUbEAUFw&index=2"
 randomdance[3]="https://www.youtube.com/watch?v=xU5ikYwR3QQ&list=RDI1gMUbEAUFw&index=7"
 randomdance[4]="https://www.youtube.com/watch?v=rpOMTJO0pQs&list=RDI1gMUbEAUFw&index=8"
 randomdance[5]="https://www.youtube.com/watch?v=ezTWZxnpBP4&list=PLF99-lxSbrkY3PuqaXaS-jAVP1qYfYEEy&index=2"
+randomdance[6]="https://www.youtube.com/watch?v=PGSJM-ZrZwg&list=PLF99-lxSbrkY3PuqaXaS-jAVP1qYfYEEy&index=3"
+randomdance[7]="https://www.youtube.com/watch?v=yXO6UPOhwC0&list=PLF99-lxSbrkY3PuqaXaS-jAVP1qYfYEEy&index=4"
+randomdance[8]="https://www.youtube.com/watch?v=GlJYTWxSVM4&list=PLF99-lxSbrkY3PuqaXaS-jAVP1qYfYEEy&index=5"
+randomdance[9]="https://www.youtube.com/watch?v=9633daDM-UY&list=PLF99-lxSbrkY3PuqaXaS-jAVP1qYfYEEy&index=6"
+
+
+
 
 function danceOnClick(){
 window.location=randomdance[Math.floor(Math.random()*randomdance.length)]
@@ -54,6 +79,12 @@ randomsing[1]="https://www.youtube.com/watch?v=nc88b7ogZQM";
 randomsing[2]="https://www.youtube.com/watch?v=9kaIT6Lu1Hk";
 randomsing[3]="https://www.youtube.com/watch?v=ujhOYGIvMhw";
 randomsing[4]="https://www.youtube.com/watch?v=SSbnDxBBvEM&list=PLKZPhIn8-5PM6v2TnKcdfd7NLqSSHGneR&index=1";
+randomsing[5]="https://www.youtube.com/watch?v=p__V39S9t_o&list=PLKZPhIn8-5PM6v2TnKcdfd7NLqSSHGneR&index=6";
+randomsing[6]="https://www.youtube.com/watch?v=_bgCMyc5LgQ&list=PLKZPhIn8-5PM6v2TnKcdfd7NLqSSHGneR&index=3";
+randomsing[7]="https://www.youtube.com/watch?v=eG7F0D5cbbs";
+randomsing[8]="https://www.youtube.com/watch?v=-EUKBafuMTg";
+randomsing[9]="https://www.youtube.com/watch?v=hav7nt_jlm0";
+
 
 
 function singOnClick(){
@@ -70,6 +101,10 @@ randomexercise[4]="https://www.youtube.com/watch?v=yKItGCpL73M"
 randomexercise[5]="https://www.youtube.com/watch?v=aPN89GOsDaI"
 randomexercise[6]="https://www.youtube.com/watch?v=2WE-L8iyu0U"
 randomexercise[7]="https://www.youtube.com/watch?v=EYDDtTJ6TtQ"
+randomexercise[8]="https://www.youtube.com/watch?v=Tz9d7By2ytQ"
+randomexercise[9]="https://www.youtube.com/watch?v=6fnLKyRJsrs"
+randomexercise[10]="https://www.youtube.com/watch?v=NMBuSMxrSaY"
+
 
 function exerciseOnClick(){
 window.location=randomexercise[Math.floor(Math.random()*randomexercise.length)]
@@ -78,10 +113,139 @@ window.location=randomexercise[Math.floor(Math.random()*randomexercise.length)]
 var randomgame=new Array();
 
 randomgame[0]="https://zyablick.github.io/sixthsite/sixthsite.html"
-// randomgame[1]="https://www.youtube.com/watch?v=AHVLT18Z1XI"
-// randomgame[2]="https://routeshuffle.com/"
-// randomgame[3]="https://www.youtube.com/watch?v=2L2lnxIcNmo"
+randomgame[1]="http://adarkroom.doublespeakgames.com/"
+randomgame[2]="https://orteil.dashnet.org/cookieclicker/"
+randomgame[3]="http://www.deconstructeam.com/games/gods-will-be-watching/"
+randomgame[4]="https://quickdraw.withgoogle.com/#"
+randomgame[5]="http://powerline.io/"
+randomgame[6]="https://www.thewikigame.com/group"
+randomgame[7]="https://www.mrtroove.com/"
+randomgame[8]="https://dinoswords.gg/"
+randomgame[9]="https://www.linerider.com/"
+
+
 
 function gameOnClick(){
 window.location=randomgame[Math.floor(Math.random()*randomgame.length)]
 }
+
+
+$(document).ready(function() {
+  // alert("Careful, you will get deep into the ocean");
+
+  $("#exercise").hover(function() {
+    $("#exercisebutton").toggle();
+    $("#exercisebc").toggle();
+
+  });
+    $("#game").hover(function() {
+      $("#gamebc").toggle();
+      $("#gamebutton").toggle();
+
+  });
+  $("#random").hover(function() {
+    $("#randombc").toggle();
+    $("#randombutton").toggle();
+
+});
+$("#dance").hover(function() {
+  $("#dancebc").toggle();
+  $("#dancebutton").toggle();
+
+});
+$("#sing").hover(function() {
+  $("#singbc").toggle();
+  $("#singbutton").toggle();
+
+});
+
+$("#myaudio").ready(function(){
+audio.volume = 0.2;
+});
+
+
+// Define a blank array for the effect positions. This will be populated based on width of the title.
+    var bArray = [];
+    // Define a size array, this will be used to vary bubble sizes
+    var sArray = [4,6,8,10];
+
+    // Push the header width values to bArray
+    for (var i = 0; i < $('.bubbles').width(); i++) {
+        bArray.push(i);
+    }
+
+    // Function to select random array element
+    // Used within the setInterval a few times
+    function randomValue(arr) {
+        return arr[Math.floor(Math.random() * arr.length)];
+    }
+
+    // setInterval function used to create new bubble every 350 milliseconds
+    setInterval(function(){
+
+        // Get a random size, defined as variable so it can be used for both width and height
+        var size = randomValue(sArray);
+        // New bubble appeneded to div with it's size and left position being set inline
+        // Left value is set through getting a random value from bArray
+        $('.bubbles').append('<div class="individual-bubble" style="left: ' + randomValue(bArray) + 'px; width: ' + size + 'px; height:' + size + 'px;"></div>');
+
+        // Animate each bubble to the top (bottom 100%) and reduce opacity as it moves
+        // Callback function used to remove finsihed animations from the page
+        $('.individual-bubble').animate({
+            'bottom': '100%',
+            'opacity' : '-=0.7'
+        }, 3000, function(){
+            $(this).remove()
+        }
+        );
+
+
+    }, 350);
+});
+
+// $(window).scroll(function () {
+//     // End of the document reached?
+//     if ($(document).height() - $(this).height() == $(this).scrollTop()) {
+//         alert('Scrolled to Bottom');
+//     }
+// });
+$(function(){
+    $(window).scroll(function(){
+           if($(document).height()<=$(window).scrollTop()+$(window).height()+100){
+               alert('end of page');
+           }
+       });
+});
+// let page = 1;
+// let currentscrollHeight: 0;
+// let lockScroll: false;
+//
+// $(window).on("scroll", () => {
+//     const scrollHeight = $(document).height() as number;
+//     const scrollPos = Math.floor($(window).height() + $(window).scrollTop());
+//     const isBottom = scrollHeight - 300 < scrollPos;
+//
+//     if (isBottom && currentscrollHeight < scrollHeight) {
+//
+//         $.ajax({
+//             method: "POST",
+//             url: "/api/search",
+//             dataType: "json",
+//             contentType: "application/json; charset=utf-8",
+//             data: JSON.stringify({ page: this.page })
+//         } as any)
+//         .done((posts: Post[]) => {
+//             this.page = this.page + 1;
+//             // do whatever
+//         });
+//
+//         currentscrollHeight = scrollHeight;
+//     }
+// });
+// $(window).on("scroll", function() {
+//   var scrollHeight = $(document).height();
+//   var scrollPos = $(window).height() + $(window).scrollTop();
+//   if ((scrollHeight - scrollPos) / scrollHeight == 0) {
+//     $('.bubbles').click();
+//     }
+// });
